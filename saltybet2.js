cfa84608512177ff81a991d8f94d287ba4c42a14
Fernando <fernandon299@gmail.com>
2018-02-28 16:57:07 -0500

    Add files via upload

---
+++ saltybet2.js
@@ -0,0 +1,7 @@
+document.styleSheets[3].insertRule("#stream{max-width:65%!important;height:75%!important;}");
+
+document.styleSheets[3].insertRule("#chat-wrapper{visibility:hidden!important;height:0px!important;width:0px!important;}");
+
+document.styleSheets[3].insertRule("#sbettorswrapper{width:35%!important;height:75%!important;float:left!important;z-index:1000;}");
+
+document.styleSheets[3].insertRule("#bottomcontent,#footer{width:100%!important;}");
