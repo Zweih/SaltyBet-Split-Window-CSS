518cae595ad47c3f6d0468c17cf67cf27d33350b
Fernando <fernandon299@gmail.com>
2018-02-28 17:11:33 -0500

    Add files via upload

---
+++ saltybet_compact_nochat.js
@@ -0,0 +1,7 @@
+document.styleSheets[3].insertRule("#stream{left:0px!important;right:35%!important;top:43px!important;bottom:219px!important;width:65%!important;max-width:none!important;}");
+
+document.styleSheets[3].insertRule("#bottomcontent{bottom:40px!important;left:0px!important;right:35%!important;width:100%!important;}");
+
+document.styleSheets[3].insertRule("#chat-wrapper{top:0%!important;width:0%!important;height:0%!important;visibility:hidden;}");
+
+document.styleSheets[3].insertRule("#sbettorswrapper{bottom:219px!important;left:65%!important;width:35%!important;font-size:14px!important;position:absolute!important;}");
